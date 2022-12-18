e8e47e5cc2cad9d2c34a5cff915f7ef20a34577f
solomon <solomonadola84@gmail.com>
2022-12-18 09:39:49 +0300

    first commit after structure

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,10 @@
+//npm packages import
 const express = require('express');
 const app = express();
+
+//local packages import
+
+//middlewares
 
 app.get('/', (req, res) => {
 	res.status(200).send('welcome,welcome this is the first part of mern blog');
