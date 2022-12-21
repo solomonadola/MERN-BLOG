f0924777ad24bcf79c9be3d047e9418a0687038d
solomon <solomonadola84@gmail.com>
2022-12-21 15:24:50 +0300

    post functionality finished

--- back-end/index.js
+++ back-end/index.js
@@ -5,23 +5,20 @@ const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
 
-
 //database
 require('./startup/db')();
-//local packages import
+//local packages router setup import
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/post');
 
 //database connection
 
-
 //middlewares
 app.use(express.json());
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
-app.use('/api/posts', postRoute);
-
+app.use('/api/post', postRoute);
 
 app.listen(5000, () => {
 	console.log('app is running on port 5000');
